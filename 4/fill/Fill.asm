e044fc12f72b1f96eeb8edcc47f6f92e4d3dceb0
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-03-08 11:05:59 +0300

    works

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -3,9 +3,112 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
-// the screen should be cleared.
+//прочитать символ
+// если 0, то установить пиксель для записи в 0
+// если не 0, то устанавливаем 1
 
-//// Replace this comment with your code.
+// от 255 до 0 //строки
+// от 512 до 0 //столбцы
+    
+@255
+D=A
+@rows
+M=D
+
+@512
+D=A
+@columns
+M=D
+
+@color
+M=0
+
+(LOOP)
+//--SELECT-COLOR---
+@KBD
+D=M
+
+@WHITE
+D;JEQ
+
+(BLACK)
+@color
+M=1
+
+@DRAW
+0;JMP
+
+(WHITE)
+@color
+M=0
+//--SELECT-COLOR---
+
+(DRAW)
+//---init-row
+@rows
+D=M-1
+@row
+M=D
+//---init-row
+
+@SCREEN
+D=A
+
+@offset
+M=D
+
+(ROWS)
+//---init-columnb
+@columns
+D=M-1
+@column
+M=D
+//---init-columnb
+
+(COLUMNS)
+
+//---FILL PIXEl---
+@color
+D=M
+
+@offset
+A=M
+M=D
+//---FILL PIXEl---
+
+@offset
+M=M+1
+
+@column
+M=M-1
+
+@COLUMNS
+M;JNE
+
+@row
+M=M-1
+
+@ROWS
+M;JNE
+
+
+@LOOP
+0;JMP
+
+(END)
+
+@SCREEN
+D=A
+
+@offset
+M=D
+
+(LOOP)
+@offset
+A=M
+M=1
+
+@offset
+M=M+1
+@LOOP
+0;JMP
